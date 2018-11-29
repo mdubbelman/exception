6c03a1d8edbe1516c53bc562e8f8bbc8b025792a
Martin Dubbelman <martindubbelman@bluebirdday.nl>
2018-11-29 08:11:15 +0100

    Changed exception message in index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 require 'vendor/autoload.php';
 
-dump(new \MD\Exception\ApplicationException('ERRORRRRR'));
+dump(new \MD\Exception\ApplicationException('A new exception'));
 echo "\n<br>@".__FILE__.':'.__LINE__ . PHP_EOL;
 die;
